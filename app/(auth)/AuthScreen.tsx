a6ea7ffdecf7bd5adeb4562dc74ab560c5de7263
VGMil <vgmiltonisaac@gmail.com>
2025-10-15 22:22:59 -0500

    ✨ feat(app/auth): agrega el formulario de Register a AuthScreen - Creo el nuevo componente RegisterForm para encapsular los campos y la lógica del formulario. - Rediseña la AuthScreen para incluir un estilo acorde al componente RegisterForm, dimension del logo y títulos. - Agrega margen negativo superior para un mejor estilo de la pantalla.

--- app/(auth)/AuthScreen.tsx
+++ app/(auth)/AuthScreen.tsx
@@ -1,4 +1,4 @@
-import LoginForm from '@/components/auth/LoginForm';
+import RegisterForm from '@/components/auth/RegisterForm';
 import CustomText from '@/components/ui/CustomText';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import React from 'react';
@@ -6,16 +6,16 @@ import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 const AuthScreen = () => {
   return (
-    <SafeAreaView className='flex-1 justify-start items-center gap-4 p-4'>
-      {/* header */}
+      <SafeAreaView className='flex-1 justify-start items-center gap-4 p-6 -mt-4'>
+        {/* header */}
       <View className='bg-teal-50 rounded-full p-4 items-center justify-center border-teal-500 border-2'>
-        <Ionicons name="musical-notes-outline" size={96} color="teal" />
+        <Ionicons name="musical-notes-outline" size={24} color="teal" /> // 24 en register / 96 en login
       </View>
-      <CustomText category='h1' color='black' className='font-bold'>inkTunes</CustomText>
-      <CustomText category="p" color='gray'>Tu Musica empieza Aqui</CustomText>
+      <CustomText category='h2' color='black' className='font-bold'>inkTunes</CustomText> // h2 en register / h1 en login
+      <CustomText category="span" color='gray' className='-mt-4'>Tu Musica empieza Aqui</CustomText> // span en register / p en login
       {/* Form */}
-      <LoginForm></LoginForm>
-
+      {/* <LoginForm></LoginForm> */}
+      <RegisterForm></RegisterForm>
     </SafeAreaView>
   )
 }
---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { TextInput, TouchableOpacity, View } from "react-native";
+import ButtonText from "../ui/ButtonText";
+import CustomText from "../ui/CustomText";
+
+const RegisterForm = () => {
+  return (
+    <View className="w-full justify-start border-black border-2 bg-white p-4 gap-2">
+      <CustomText>Nombre completo</CustomText>
+      <TextInput 
+        placeholder="Nombre Apellido"
+        className="border-2 focus:border-primary focus:ring-primary"
+      />
+      <CustomText>Email</CustomText>
+      <TextInput 
+        placeholder="tu@email.com"
+        className="border-2 focus:border-primary focus:ring-primary"
+      />
+      <CustomText>Contraseña</CustomText>
+      <TextInput 
+        placeholder="*********"
+        className="border-2 focus:border-primary focus:ring-primary"
+      />
+      <CustomText>Confirmar contraseña</CustomText>
+      <TextInput 
+        placeholder="*********"
+        className="border-2 focus:border-primary focus:ring-primary"
+      />
+      <View>
+        {/* CheckBox para términos y condiciones */}
+        <CustomText color="gray" category="span">
+          Al registrarte, aceptas nuestros{' '}
+            <CustomText color="teal" category="link">
+                términos y condiciones
+            </CustomText>
+        </CustomText>
+
+      </View>
+      <ButtonText text="Registrarse" />
+      <View className="flex-row justify-center items-center gap-4 w-full ">
+        <View className="w-1/4 border-t-2 border-border" />
+        <CustomText color="gray" category="span">
+          o continua con
+        </CustomText>
+        <View className="w-1/4 border-t-2 border-border" />
+      </View>
+      <View className="flex-row gap-8 p-4">
+        <ButtonText category="secondary" text="Google" className="flex-1"/>
+        <ButtonText category="secondary" text="Apple" className="flex-1"/>
+      </View>
+      <CustomText color="gray" category="span" className="self-center">
+        ¿Ya tienes una cuenta?
+      </CustomText>
+      <TouchableOpacity>
+        <CustomText color="teal" category="span" className="self-center">
+          Inicia sesión
+        </CustomText>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default RegisterForm;

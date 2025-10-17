bccba4e5882f7cddc1b91da7595a618593113200
vgmil <vgmiltonisaac@gmail.com>
2025-10-17 10:14:41 -0500

    🔧 chore(config): agrega soporte para SVG y actualiza dependencias - Configura `metro.config.js` para manejar archivos SVG. - Actualiza `package.json` y `package-lock.json` con nuevas dependencias:     - react-native-svg ^15.12.1     - react-native-svg-transformer ^1.5.1 - Modifica `tsconfig.json` para incluir la declaración de tipos SVG. - Agrega `types/svg.d.ts` para la tipificación de módulos SVG.

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+const config = getDefaultConfig(__dirname);
+
+// Configuración para SVG transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
+// Aplicar NativeWind al final
+module.exports = withNativeWind(config, { input: './global.css' });

e5c5313256e5171a4d61e84550a2a68dcaa4b288
VGMil <vgmiltonisaac@gmail.com>
2025-10-17 03:01:02 -0500

    ✨ feat(app/auth): anima el intercambio de formularios con Reanimated - Implementa una animación de "Slide" para la transición entre los formularios de login y registro. - Implementa una animación de "Grow" para el icono de inicio de sesión. - Utiliza `react-reanimated` para crear una experiencia de usuario atractiva al cambiar de formulario. - Renombra la variable de estado `isLogin` a `handleForm` para un nombre más descriptivo que refleje su función.

--- app/(auth)/AuthScreen.tsx
+++ app/(auth)/AuthScreen.tsx
@@ -2,35 +2,60 @@ import LoginForm from '@/components/auth/LoginForm';
 import RegisterForm from '@/components/auth/RegisterForm';
 import CustomText from '@/components/ui/CustomText';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
-const AuthScreen = () => {
 
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+const AnimatedIonicon = Animated.createAnimatedComponent(Ionicons);
+
+const AuthScreen = () => {
+  const [handleForm, setHandleForm] = useState<boolean>(false);
+  const translate = useSharedValue(2000);
+  const iconFont = useSharedValue(24);
+
+  useEffect(() => {
+    translate.value = withTiming(0, {
+      duration: 500,
+      easing: Easing.inOut(Easing.quad)
+    })
+    iconFont.value = withTiming(handleForm ? 96 : 24, {
+      duration: 500,
+      easing: Easing.inOut(Easing.quad)
+    })
+
+    return () => {
+      translate.value = 2000;
+    }
+  }, [handleForm])
+
+  const handleTranslate = useAnimatedStyle(() => {return {transform: [{translateY: translate.value}]}});
+  const handleIconFont = useAnimatedStyle(() => {return {fontSize: iconFont.value}});;
 
   return (
-      <SafeAreaView className='flex-1 justify-start items-center gap-4 p-6 -mt-4'>
-        {/* header */}
+    <SafeAreaView className='flex-1 justify-start items-center gap-4 p-6 -mt-4'>
+      {/* header */}
       <View className='bg-teal-50 rounded-full p-4 items-center justify-center border-teal-500 border-2'>
-         {/* // 24 en register / 96 en login */}
-        <Ionicons name="musical-notes-outline" size={isLogin?96:24} color="teal" />
+        {/* // 24 en register / 96 en login */}
+        <AnimatedIonicon name="musical-notes-outline" color="teal" style={handleIconFont} />
       </View>
-       {/* // h2 en register / h1 en login */}
-      <CustomText category={(isLogin)?'h1':'h2'} color='black' className='font-bold'>inkTunes</CustomText>
+      {/* // h2 en register / h1 en login */}
+      <CustomText category={(handleForm) ? 'h1' : 'h2'} color='black' className='font-bold'>inkTunes</CustomText>
       {/* // span en register / p en login */}
-      <CustomText category={(isLogin)?'p':'span'} color='gray' className='-mt-4'>Tu Musica empieza Aqui</CustomText> 
+      <CustomText category={(handleForm) ? 'p' : 'span'} color='gray' className='-mt-4'>Tu Musica empieza Aqui</CustomText>
       {/* Form */}
-      {
-        (isLogin)?
-          <LoginForm 
-          changeToRegister={() => setIsLogin(false)}
-          />
-        :
-          <RegisterForm
-          changeToLogin={() => setIsLogin(true)}
-          />
-      }
+      <Animated.View style={handleTranslate}>
+        {
+          (handleForm) ?
+            <LoginForm
+              changeToRegister={() => setHandleForm(false)}
+            />
+            :
+            <RegisterForm
+              changeToLogin={() => setHandleForm(true)}
+            />
+        }
+      </Animated.View>
     </SafeAreaView>
   )
 }
